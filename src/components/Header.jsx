208f36541187bc8beb4e318f967f64a0aa2feaa2
snowleopardAlexa <alexaslomski@gmail.com>
2022-04-14 20:51:15 -0400

    create list and style list element Home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,26 @@ import logo from '../img/cat-logo.svg'
 const Header = () => {
   return (
     <nav className="bg-teal-600 sm:px-4 py-2.5">
-      <div className="">
-        <a href="https://..." class="flex items-center">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <a href="https://..." className="flex items-center">
           <img
-            className="rounded-[52px]"
+            className="ml-2 md:ml-0 rounded-[52px]"
             src={logo}
             width="80px"
             height="80px"
             alt="cat logo"
           />
-          <span class="ml-4 font-Bebas self-center text-4xl font-semibold whitespace-nowrap dark:text-white">
+          <span class="ml-4 mt-3 font-Bebas self-center text-3xl md:text-4xl font-semibold whitespace-nowrap dark:text-white">
             Cats Videos App
           </span>
         </a>
+        <div>
+            <ul className="md:flex flex-col">
+                <li>
+                    <a href="https://..." className="text-white block pr-4 mt-2 md:pr-0 text-1xl md:text-2xl font-Lato font-bold hover:text-yellow-500">Home</a>
+                </li>
+            </ul>
+        </div>
       </div>
     </nav>
   );
