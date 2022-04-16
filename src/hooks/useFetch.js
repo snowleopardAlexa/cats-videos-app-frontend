e353f68ee7862d95c0491df5a9c53de0b46b8bde
snowleopardAlexa <alexaslomski@gmail.com>
2022-04-16 19:55:10 -0400

    add useState - setData, setError, setLoading

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react'
 
 const useFetch = () => {
+  const [data, setData] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
 
+
+  return { loading, error, data }
 }
 
 export default useFetch
