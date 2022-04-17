6418bf5cccec3def0120618a5e3a1ea15581e117
snowleopardAlexa <alexaslomski@gmail.com>
2022-04-16 20:03:41 -0400

    import useFetch to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,11 @@
-// video list
+import useFetch from '../hooks/useFetch'
 import VideoCard from "../components/VideoCard";
 
 const HomePage = () => {
+  const { loading, error, data } = useFetch('http://localhost:1337/videos')
+
+  
+
   return (
     <> 
      <h2 className="text-5xl mt-8 text-center font-Bebas">Cats Videos</h2>
